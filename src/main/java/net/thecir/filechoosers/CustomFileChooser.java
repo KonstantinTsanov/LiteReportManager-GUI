d45d8703d880e48853f96301e772a6a59ff66ba8
Konstantin Tsanov <k.tsanov@gmail.com>
2017-07-16 23:49:29 +0300

    Added filter.

--- src/main/java/net/thecir/filechoosers/CustomFileChooser.java
+++ src/main/java/net/thecir/filechoosers/CustomFileChooser.java
@@ -26,6 +26,7 @@ package net.thecir.filechoosers;
 import java.io.File;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.filechooser.FileFilter;
 import net.thecir.callbacks.FileCallback;
 
 /**
@@ -38,13 +39,30 @@ public class CustomFileChooser extends JFileChooser implements FileCallback {
 
     public CustomFileChooser(JFrame parent) {
         this.parent = parent;
+        setFileSelectionMode(FILES_ONLY);
+        setFileFilter(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                if (file.isDirectory()) {
+                    return true;
+                } else {
+                    String filename = file.getName().toLowerCase();
+                    return filename.endsWith(".xslx");
+                }
+            }
+
+            @Override
+            public String getDescription() {
+                return "Excel Workbook (*.xlsx)";
+            }
+        });
     }
 
     @Override
     public File getFile() {
-        int result = showOpenDialog(parent);
+        int result = showSaveDialog(parent);
         if (result == JFileChooser.APPROVE_OPTION) {
-            return this.getSelectedFile();
+            return getSelectedFile();
         } else {
             return null;
         }
