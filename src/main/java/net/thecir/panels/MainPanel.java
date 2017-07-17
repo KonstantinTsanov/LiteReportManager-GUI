b678f0e38bdce6234f8c03ebce0367bb8b8b1b76
Konstantin Tsanov <k.tsanov@gmail.com>
2017-07-17 23:40:33 +0300

    Minor fixes.

--- src/main/java/net/thecir/panels/MainPanel.java
+++ src/main/java/net/thecir/panels/MainPanel.java
@@ -27,50 +27,58 @@ import java.util.ResourceBundle;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import net.miginfocom.swing.MigLayout;
+import net.thecir.core.LiteReportManager;
+import net.thecir.filechoosers.CustomFileChooser;
+import net.thecir.filemanagers.NewFileManager;
 
 /**
  *
  * @author Konstantin Tsanov <k.tsanov@gmail.com>
  */
 public class MainPanel extends JPanel {
-    
+
     private JLabel srcFileLabel;
     private JLabel destFileLabel;
-    
+
     private JTextField srcFilePath;
     private JTextField destFilePath;
-    
+
     private JButton selectSrcFileButton;
     private JButton selectDestFileButton;
     private JButton createNewFileButton;
     private JButton clearSrcFileButton;
     private JButton clearDestFileButton;
-    
+
     private JRadioButton technopolisJButton;
     private JRadioButton technomarketJButton;
     private ButtonGroup retailersButtonGroup;
-    
+
     private JButton generateReport;
     private JButton cancelButton;
 
     //If the data from the input file must be subtracted from the output file rather than added to it
     private JCheckBox subtractCheckBox;
-    
+
     private JLabel statusLabel;
     private JTextArea statusTextArea;
-    
-    public MainPanel() {
+
+    private JFrame parent;
+
+    public MainPanel(JFrame parent) {
+        this.parent = parent;
         MigLayout layout = new MigLayout("", "[shrink 0][grow][shrink 0][shrink 0]", "[shrink 0][shrink 0][shrink 0][shrink 0][shrink 0]");
         setLayout(layout);
         initComponents();
         addComponents();
         setComponentText();
+        attachListeners();
     }
 
     /**
@@ -79,35 +87,35 @@ public class MainPanel extends JPanel {
     private void initComponents() {
         srcFileLabel = new JLabel();
         destFileLabel = new JLabel();
-        
+
         srcFilePath = new JTextField();
         srcFilePath.setEditable(false);
-        
+
         destFilePath = new JTextField();
         destFilePath.setEditable(false);
-        
+
         selectSrcFileButton = new JButton("...");
         selectDestFileButton = new JButton("...");
         createNewFileButton = new JButton("...");
         clearSrcFileButton = new JButton();
         clearDestFileButton = new JButton();
-        
+
         technopolisJButton = new JRadioButton();
         technomarketJButton = new JRadioButton();
         retailersButtonGroup = new ButtonGroup();
         retailersButtonGroup.add(technomarketJButton);
         retailersButtonGroup.add(technopolisJButton);
-        
+
         generateReport = new JButton();
         cancelButton = new JButton();
         subtractCheckBox = new JCheckBox();
-        
+
         statusLabel = new JLabel();
         statusTextArea = new JTextArea();
         statusTextArea.setEditable(false);
         statusTextArea.setOpaque(true);
     }
-    
+
     private void addComponents() {
         add(srcFileLabel);
         add(srcFilePath, "growx");
@@ -126,7 +134,7 @@ public class MainPanel extends JPanel {
         add(statusLabel);
         add(statusTextArea, "span, growx");
     }
-    
+
     public void setComponentText() {
         ResourceBundle r = ResourceBundle.getBundle("Bundle");
         srcFileLabel.setText(r.getString("MainPanel.srcFileLabel"));
@@ -140,5 +148,12 @@ public class MainPanel extends JPanel {
         cancelButton.setText(r.getString("MainPanel.cancelButton"));
         subtractCheckBox.setText(r.getString("MainPanel.subtractCheckBox"));
         statusLabel.setText(r.getString("MainPanel.statusLabel"));
+    }
+
+    private void attachListeners() {
+        createNewFileButton.addActionListener((ae) -> {
+            NewFileManager.getInstance().setFileCallback(new CustomFileChooser(parent));
+            LiteReportManager.getInstance().createNewFile();
+        });
     }
 }
